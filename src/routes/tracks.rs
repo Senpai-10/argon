eef44bbb4a05dc15aa881227a18f7b86d3d61d2d
abdalrzag eisa <bmjfdrh@gmail.com>
2023-09-04 18:07:01 +0300

    Update `track.plays` in stream route

--- src/routes/tracks.rs
+++ src/routes/tracks.rs
@@ -86,13 +86,22 @@ pub fn track(id: String) -> Json<Response<TrackData>> {
 pub fn track_stream<'a>(id: String) -> Result<SeekStream<'a>, NotFound<String>> {
     let mut conn = db::establish_connection();
 
-    let track = match schema::tracks::table
+    let track: Track = match schema::tracks::table
         .filter(schema::tracks::id.eq(&id))
         .get_result::<Track>(&mut conn)
     {
         Ok(v) => v,
         Err(e) => return Err(NotFound(e.to_string())),
     };
+
+    // Update track global plays
+    if let Err(e) = diesel::update(schema::tracks::table)
+        .filter(schema::tracks::id.eq(&id))
+        .set(schema::tracks::plays.eq(track.plays + 1))
+        .execute(&mut conn)
+    {
+        error!("Failed to update track plays!, {e}")
+    }
 
     let file = Path::new(&track.path);
 
