c01758233f416d30bb212088a98fc6252b008779
abdalrzag eisa <bmjfdrh@gmail.com>
2023-09-06 19:32:32 +0300

    Paginated search results

--- src/routes/search.rs
+++ src/routes/search.rs
@@ -32,54 +32,85 @@ pub struct AlbumsSearchData {
     albums: Vec<AlbumWithTracks>,
 }
 
-#[get("/search?<q>")]
-pub async fn search(q: String) -> Json<Response<SearchAllData>> {
+#[get("/search?<q>&<offset>&<limit>")]
+pub async fn search(
+    q: String,
+    offset: Option<i64>,
+    limit: Option<i64>,
+) -> Json<Response<SearchAllData>> {
     let mut conn = db::establish_connection();
 
     let search_query = format!("%{q}%");
 
     Json(Response::data(SearchAllData {
-        artists: get_artists(&mut conn, &search_query),
-        tracks: get_tracks(&mut conn, &search_query),
-        albums: get_albums(&mut conn, &search_query),
+        artists: get_artists(&mut conn, &search_query, offset, limit),
+        tracks: get_tracks(&mut conn, &search_query, offset, limit),
+        albums: get_albums(&mut conn, &search_query, offset, limit),
     }))
 }
 
-#[get("/search/artists?<q>")]
-pub async fn search_artists(q: String) -> Json<Response<ArtistsSearchData>> {
+#[get("/search/artists?<q>&<offset>&<limit>")]
+pub async fn search_artists(
+    q: String,
+    offset: Option<i64>,
+    limit: Option<i64>,
+) -> Json<Response<ArtistsSearchData>> {
     let mut conn = db::establish_connection();
 
     let search_query = format!("%{q}%");
 
     Json(Response::data(ArtistsSearchData {
-        artists: get_artists(&mut conn, &search_query),
+        artists: get_artists(&mut conn, &search_query, offset, limit),
     }))
 }
 
-#[get("/search/tracks?<q>")]
-pub async fn search_tracks(q: String) -> Json<Response<TracksSearchData>> {
+#[get("/search/tracks?<q>&<offset>&<limit>")]
+pub async fn search_tracks(
+    q: String,
+    offset: Option<i64>,
+    limit: Option<i64>,
+) -> Json<Response<TracksSearchData>> {
     let mut conn = db::establish_connection();
 
     let search_query = format!("%{q}%");
 
     Json(Response::data(TracksSearchData {
-        tracks: get_tracks(&mut conn, &search_query),
+        tracks: get_tracks(&mut conn, &search_query, offset, limit),
     }))
 }
 
-#[get("/search/albums?<q>")]
-pub async fn search_albums(q: String) -> Json<Response<AlbumsSearchData>> {
+#[get("/search/albums?<q>&<offset>&<limit>")]
+pub async fn search_albums(
+    q: String,
+    offset: Option<i64>,
+    limit: Option<i64>,
+) -> Json<Response<AlbumsSearchData>> {
     let mut conn = db::establish_connection();
 
     let search_query = format!("%{q}%");
 
     Json(Response::data(AlbumsSearchData {
-        albums: get_albums(&mut conn, &search_query),
+        albums: get_albums(&mut conn, &search_query, offset, limit),
     }))
 }
 
-fn get_artists(conn: &mut PgConnection, search_query: &str) -> Vec<ArtistWithTracks> {
-    let all_artists = schema::artists::table
+fn get_artists(
+    conn: &mut PgConnection,
+    search_query: &str,
+    offset: Option<i64>,
+    limit: Option<i64>,
+) -> Vec<ArtistWithTracks> {
+    let mut query = schema::artists::table.into_boxed();
+
+    if let Some(offset) = offset {
+        query = query.offset(offset);
+    }
+
+    if let Some(limit) = limit {
+        query = query.limit(limit);
+    }
+
+    let all_artists = query
         .filter(schema::artists::name.ilike(&search_query))
         .select(Artist::as_select())
         .load(conn)
@@ -113,11 +144,27 @@ fn get_artists(conn: &mut PgConnection, search_query: &str) -> Vec<ArtistWithTra
         .collect::<Vec<ArtistWithTracks>>()
 }
 
-fn get_tracks(conn: &mut PgConnection, search_query: &str) -> Vec<TrackInRes> {
-    schema::tracks::table
+fn get_tracks(
+    conn: &mut PgConnection,
+    search_query: &str,
+    offset: Option<i64>,
+    limit: Option<i64>,
+) -> Vec<TrackInRes> {
+    let mut query = schema::tracks::table
         .filter(schema::tracks::title.ilike(&search_query))
         .left_join(schema::artists::table)
         .left_join(schema::albums::table)
+        .into_boxed();
+
+    if let Some(offset) = offset {
+        query = query.offset(offset);
+    }
+
+    if let Some(limit) = limit {
+        query = query.limit(limit);
+    }
+
+    query
         .load::<(Track, Option<Artist>, Option<Album>)>(conn)
         .unwrap()
         .into_iter()
@@ -136,8 +183,23 @@ fn get_tracks(conn: &mut PgConnection, search_query: &str) -> Vec<TrackInRes> {
         .collect::<Vec<TrackInRes>>()
 }
 
-fn get_albums(conn: &mut PgConnection, search_query: &str) -> Vec<AlbumWithTracks> {
-    let all_albums = schema::albums::table
+fn get_albums(
+    conn: &mut PgConnection,
+    search_query: &str,
+    offset: Option<i64>,
+    limit: Option<i64>,
+) -> Vec<AlbumWithTracks> {
+    let mut query = schema::albums::table.into_boxed();
+
+    if let Some(offset) = offset {
+        query = query.offset(offset);
+    }
+
+    if let Some(limit) = limit {
+        query = query.limit(limit);
+    }
+
+    let all_albums = query
         .filter(schema::albums::title.ilike(&search_query))
         .select(Album::as_select())
         .load(conn)
